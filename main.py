8da6ab3962e23d748051b2ff0a522b7037c35427
Gabriel Molina <g-abox@hotmail.com>
2024-02-24 16:55:07 -0300

    pestaña pinchaduras funcional

--- main.py
+++ main.py
@@ -127,18 +127,25 @@ KV = '''
             MDTopAppBar:
                 title: "[size="+app.wresize["bar_fsize"]+"]Medidas[/size]"
                 anchor_title: "left"
-                # right_action_items: [["content-save", lambda x: root.save_mes()]]
+                # right_action_items: [["content-save", lambda x: root.save_mes_med()]]
                 pos_hint: {'top': 1}
             MDIconButton:
                 icon: "content-save"
                 pos_hint: {'right': .99, "center_y": .65}
                 icon_size: app.wresize["bar_fsize"]
-                on_press: root.save_mes()
+                on_press: root.save_mes("corp_mes")
             MDIconButton:
                 icon: "test-tube"
                 pos_hint: {'right': .83, "center_y": .65}
                 icon_size: app.wresize["bar_fsize"]
                 on_press: root.test_db()
+            MDIconButton:
+                icon: "bicycle"
+                pos_hint: {'right': .68, "center_y": .65}
+                icon_size: app.wresize["bar_fsize"]
+                on_press:
+                    app.root.transition = SlideTransition(direction="right")
+                    app.root.current = "bike_notes"        
         BoxLayout:
             size_hint: 1, .9
             orientation: 'vertical'
@@ -146,6 +153,36 @@ KV = '''
             ScrollView:
                 MDList:
                     id: input_fields
+                    spacing: 10
+    Screen:
+        name: "bike_notes"
+        FloatLayout:
+            size_hint: 1, .13
+            pos_hint: {'top': 1}
+            MDTopAppBar:
+                title: "[size="+app.wresize["bar_fsize2"]+"]Pinchadura Bicicl.[/size]"
+                anchor_title: "left"
+                # right_action_items: [["content-save", lambda x: root.save_mes_bike()]]
+                pos_hint: {'top': 1}
+            MDIconButton:
+                icon: "content-save"
+                pos_hint: {'right': .99, "center_y": .65}
+                icon_size: app.wresize["bar_fsize"]
+                on_press: root.save_mes("bike_notes")
+            MDIconButton:
+                icon: "scale-balance"
+                pos_hint: {'right': .83, "center_y": .65}
+                icon_size: app.wresize["bar_fsize"]
+                on_press:
+                    app.root.transition = SlideTransition(direction="left")
+                    app.root.current = "corp_mes"      
+        BoxLayout:
+            size_hint: 1, .9
+            orientation: 'vertical'
+            padding: 15
+            ScrollView:
+                MDList:
+                    id: input_bike
                     spacing: 10
 '''
 
@@ -220,7 +257,8 @@ class ScManag(MDScreenManager):
         self.auth = self.fbase.auth()
         self.db = self.fbase.db()
         ## DB directory
-        self.db_node_target = self.config["pyrebase"]["db_node"]
+        self.db_node_target1 = self.config["pyrebase"]["db_node1"]
+        self.db_node_target2 = self.config["pyrebase"]["db_node2"]
         ## verify store user
         self.user = None
         self.mail = self.config["user"]["mail"]
@@ -239,8 +277,8 @@ class ScManag(MDScreenManager):
             self.db_url = self.config["firebase"]["url"]
             self.data_name = self.config["firebase"]["data_name"]
         
-        # input list declaration
-        for  text in ["Peso (g)",
+        # input list declaration (Screen: 'corp_mes')
+        for text in ["Peso (g)",
              "Diámetro SO max (cm)",
              "Diámetro SO max (cm)",
              "Diámetro BO max (cm)",
@@ -260,6 +298,28 @@ class ScManag(MDScreenManager):
             self.ids.input_fields.add_widget(
                 MDLabel(size_hint=(.7, .08))
             )
+        
+        # input list declaration (Screen: 'bike_notes')
+        for text in ["Fecha (dd/mm/aa)",
+             "Bicicleta",
+             "Rueda",
+             "Cámara",
+             "n° Pinchaduras",
+             "Cámara reemplazo"]:
+            self.ids.input_bike.add_widget(
+                MDTextField(
+                    size_hint=(.7, .08),
+                    mode="rectangle",
+                    font_size=self.app.wresize["bar_fsize"],
+                    hint_text=text,
+                    line_color_normal=self.app.theme_cls.accent_color
+                )
+            )
+        for i in range(50):
+            self.ids.input_bike.add_widget(
+                MDLabel(size_hint=(.7, .08))
+            )
+
     # authentication methods (Screens: 'auth_sign' & 'auth_regist')
     def sign_in(self):
         print("sign_in:", self.user_mail, self.user_pwd)
@@ -294,8 +354,28 @@ class ScManag(MDScreenManager):
         else:
             print("CONFIRMACIÓN DE CONTRASEÑA NO COINCIDEN")
     
-    # Screen: 'corp_mes' methods        ####
-    def save_mes(self):
+    # input Screens methods #################################################
+    def show_note(self,note:str, durat=3):
+        '''
+        Float snackbar based popup.
+        '''
+        print(self.app.wresize["warg_font_s"])
+        MDSnackbar(
+            MDLabel(
+                text=f'[size={self.app.wresize["warg_font_s"]}\
+]  {note}[/size]',
+                markup=True,
+                bold=True,
+                halign= "center"
+            ),
+            duration=durat,
+            y=dp(24),
+            pos_hint={"center_x": 0.5, "top":.8},
+            size_hint_x=0.8,
+            radius=[20,20,20,20]           
+        ).open()        
+    
+    def save_mes_med(self):
         '''Send to database using `request.post` method.
         A timestamp is included along with the data 
         (`datetime` module. UTC).
@@ -342,39 +422,84 @@ class ScManag(MDScreenManager):
             met1=alta
         )
         
-    def show_note(self,note:str, durat=3):
-        '''
-        Float snackbar based popup.
-        '''
-        print(self.app.wresize["warg_font_s"])
-        MDSnackbar(
-            MDLabel(
-                text=f'[size={self.app.wresize["warg_font_s"]}\
-]  {note}[/size]',
-                markup=True,
-                bold=True,
-                halign= "center"
-            ),
-            duration=durat,
-            y=dp(24),
-            pos_hint={"center_x": 0.5, "top":.8},
-            size_hint_x=0.8,
-            radius=[20,20,20,20]           
-        ).open()
-        
     def test_db(self):
         '''Switch between defoult and test nodes of Firebase DB.
         (for development purposes only)'''
         cfg = self.config["pyrebase"]
-        print(self.db_node_target, "to:")
-        if self.db_node_target == cfg["db_node"]:
-            self.db_node_target = cfg["db_node_pr"]
+        print(self.db_node_target1, "to:")
+        if self.db_node_target1 == cfg["db_node1"]:
+            self.db_node_target1 = cfg["db_node_pr"]
         else:
-            self.db_node_target = cfg["db_node"]
-        print(self.db_node_target)
-        self.show_note(f"A: {self.db_node_target}", durat=.2)
+            self.db_node_target1 = cfg["db_node1"]
+        print(self.db_node_target1)
+        self.show_note(f"A: {self.db_node_target1}", durat=.2)
+
+    def save_mes(self, mode:str):
+        '''Send to database using `request.post` method.
+        A timestamp is included along with the data 
+        (`datetime` module. UTC).
+        '''
+        if mode == "corp_mes":
+            conteiner = self.ids.input_fields.children
+            db_node = self.db_node_target1
+        elif mode == "bike_notes":
+            conteiner = self.ids.input_bike.children
+            db_node = self.db_node_target2
+        # Retrive text from ScrollView's MDTextField childrens
+        data_list = []
+        for child in reversed(conteiner):
+            if isinstance(child, MDTextField):
+                data_list.append(child.text)
+        print("DATA LIST:",data_list)
+        timestamp = str(datetime.now(timezone.utc)).replace(" ", "_")
         
-        
+        if mode == "corp_mes":
+            data ={
+                "timestamp":timestamp,
+                "medidas":{
+                    "peso":data_list[0],
+                    "dso_mx":data_list[1],
+                    "dso_mn":data_list[2],
+                    "dbo_mx":data_list[3],
+                    "dbo_mn":data_list[4]
+                    }
+                }
+        elif mode == "bike_notes":
+            data ={
+                "timestamp":timestamp,
+                "notas_bicicleta":{
+                    "Fecha":data_list[0],
+                    "Bicicleta":data_list[1],
+                    "Rueda":data_list[2],
+                    "Cámara":data_list[3],
+                    "n° Pinchaduras":data_list[4],
+                    "Cámara reemplazo":data_list[5]
+                    }
+                }
+            
+        def alta(*args):
+            '''Send data to Firebase DB.'''
+            # `self.user['idToken']` mandatory for user verification
+            try:
+                results = self.db.child(db_node).child(
+                    datetime.now().strftime("%d-%m-%y(%H:%M:%S)")
+                        ).set(data, self.user['idToken'])
+                self.show_note("Datos enviados.")
+                print("RESPUESTA:")
+                print(results)       
+            except:
+                self.show_note("No se pudo enviar.")
+            self.app.close_warng()
+
+        self.app.close_warng()
+        self.app.warning(
+            text="¿Enviar medidas?",
+            ok_txt="Enviar",
+            dism_txt="Cancelar",
+            met1=alta
+        )
+    
+
 class MedidasApp(MDApp):
     
     COLS = DictProperty()
@@ -397,7 +522,8 @@ class MedidasApp(MDApp):
             
     def on_resize(self, *args):
         '''Al cambiar dimensiones de ventana'''
-        self.wresize["bar_fsize"] = str(int(Window.size[1]/15))
+        self.wresize["bar_fsize"] = str(int(Window.size[1]/18))
+        self.wresize["bar_fsize2"] = str(int(Window.size[1]/25))
         self.wresize["input_font_s"] = str(int(Window.size[1]/15))
         self.wresize["warg_font_s"] = str(int(Window.size[1]/30))
         self.wresize["titl_font_s"] = str(int(Window.size[1]/25))
